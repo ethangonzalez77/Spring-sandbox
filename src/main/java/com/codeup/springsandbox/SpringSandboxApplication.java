c11ff25585f61844d410d60aa644b235ea59b509
ethan <ethan.gonzalez00774@gmail.com>
2023-01-18 19:34:14 -0800

    The FIRST Commit

---
+++ src/main/java/com/codeup/springsandbox/SpringSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.springsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/springsandbox/SpringSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.springsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
